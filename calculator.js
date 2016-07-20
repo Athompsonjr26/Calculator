5741dbd9f8fd88d8c063ee750f5129c7bd18a3f9
Anthony Thompson <athompsonjr26@gmail.com>
2016-07-20 08:57:37 -0400

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,51 +1,56 @@
 
-var currentNumber;
 var operator;
+var currentNumber;
+var display = '';
 
-
-$(function () {
+$(function() {
   $('.number').click(function () {
-    var number = $(this).text();
-    var currentDisplay = $('#display').val();
-    var newDisplay = currentDisplay + number;
-    $('#display').val(newDisplay);
+    var digit = $(this).text();
+    display += digit;
+    $('#display').val(display);
   });
-
   $('.operator').click(function () {
-    var newNumber = Number($('#display').val());
-    var result;
-    if (operator && currentNumber) {
+    var newNumber = Number(display);
+    if (currentNumber && operator) {
       if (operator === '+') {
-        result = newNumber + currentNumber;
+        currentNumber = currentNumber + newNumber;
       } else if (operator === '-') {
-        result = currentNumber - newNumber;
+        currentNumber = currentNumber - newNumber;
       } else if (operator === 'x') {
-        result = currentNumber * newNumber;
+        currentNumber = currentNumber * newNumber;
       } else if (operator === '/') {
-        result = currentNumber / newNumber;
+        currentNumber = currentNumber / newNumber;
       }
-      currentNumber = result;
     } else {
       currentNumber = newNumber;
     }
-    operator = $(this).text();
-    $('#display').val('');
+    operator = $(this).text(); // new operator
+    display = '';
+    $('#display').val(display);
   });
-
   $('.evaluate').click(function () {
-    var newNumber = Number($('#display').val());
-    var result;
-    if (operator && currentNumber) {
+    var newNumber = Number(display);
+    if (currentNumber && operator) {
       if (operator === '+') {
-        result = newNumber + currentNumber;
+        currentNumber = currentNumber + newNumber;
       } else if (operator === '-') {
-        result = currentNumber - newNumber;
+        currentNumber = currentNumber - newNumber;
       } else if (operator === 'x') {
-        result = currentNumber * newNumber;
+        currentNumber = currentNumber * newNumber;
       } else if (operator === '/') {
-        result = currentNumber / newNumber;
+        currentNumber = currentNumber / newNumber;
       }
-      $('#display').val(result);
+    } else {
+      currentNumber = newNumber;
     }
+    operator = null;
+    display = currentNumber;
+    $('#display').val(display);
+  });
+  $('#clear').click(function () {
+    display = '';
+    operator = null;
+    currentNumber = null;
+    $('#display').val(display);
   });
 });
